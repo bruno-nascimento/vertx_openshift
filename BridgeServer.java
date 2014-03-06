6b340064918eb5fe602aa254ff3061edc9eb5b20
Bruno Nascimento <brunocn80@gmail.com>
2014-03-06 10:36:27 -0300

    colocando logs em todos os cantos para entender oq eu estou fazendo de errado

--- BridgeServer.java
+++ BridgeServer.java
@@ -2,8 +2,8 @@ package repo;
 
 import java.util.Random;
 
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
@@ -17,9 +17,9 @@ import org.vertx.java.platform.Verticle;
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class BridgeServer extends Verticle {
-  Logger logger;
+	Logger logger;
 
-  public void start() {
+	public void start() {
     logger = container.logger();
     
     HttpServer server = vertx.createHttpServer();
@@ -27,7 +27,19 @@ public class BridgeServer extends Verticle {
     // Also serve the static resources. In real life this would probably be done by a CDN
     server.requestHandler(new Handler<HttpServerRequest>() {
       public void handle(HttpServerRequest req) {
-        if (req.path().equals("/")) req.response().sendFile("webroot/index.html"); // Serve the index.html
+    	  req.exceptionHandler(new Handler<Throwable>() {
+			@Override
+			public void handle(Throwable err) {
+				logger.fatal("OPAAAAAAAAAAAAAAAA deu merda!", err);
+			}
+		});
+        if (req.path().equals("/")) req.response().sendFile("webroot/index.html", "OPA! cadê isso que vocÊ pediu? tava aqui agorinha ... sumiu!", 
+        		new Handler<AsyncResult<Void>>() {
+					@Override
+					public void handle(AsyncResult<Void> paramE) {
+						logger.info("@@@@@@@@ handle sendFile index"+ paramE);
+					}
+        		}); // Serve the index.html
       }
     });
 
@@ -43,9 +55,17 @@ public class BridgeServer extends Verticle {
     
     
     String host = System.getenv("OPENSHIFT_VERTX_IP");
-    String port = System.getenv("OPENSHIFT_VERTX_PORT");
+    logger.info("@@@@@@@@@ env.host: "+ host);
     
-    server.listen(port, host);
+    String port = System.getenv("OPENSHIFT_VERTX_PORT");
+    logger.info("@@@@@@@@@ env.port: "+ port);
+    
+    server.listen(port!= null ? Integer.valueOf(port) : 8080, host != null ? host : "127.0.0.1", new Handler<AsyncResult<HttpServer>>() {
+		@Override
+		public void handle(AsyncResult<HttpServer> paramE) {
+			logger.info("handler do listen do server: "+paramE);
+		}
+	});
     
     final EventBus eb = vertx.eventBus();
     final Random gerador = new Random();
